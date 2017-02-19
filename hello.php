9e533247b885c25e6344bcbd520cb6b17c557a7c
Marcel T <mardroid@users.noreply.github.com>
2017-02-19 11:41:56 +0100

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,49 +1,62 @@
 <?php
 /**
- * @package Hello_Dolly
- * @version 1.6
+ * @package focus_hocus_pocus
+ * @version 1.0
  */
 /*
-Plugin Name: Hello Dolly
-Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Matt Mullenweg
-Version: 1.6
-Author URI: http://ma.tt/
+Plugin Name: Focus - Hocus Pocus
+Plugin URI: https://wordpress.org/plugins/focus-hocus-pocus
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Thijs van Leer from Focus: Hocus Pocus. When activated you will randomly see a lyric from <cite>Hocus Pocus</cite> in the upper right of your admin screen on every page.
+Author: Tannich Marcel
+Version: 1.0
+Author URI: http://tannich.com
 */
 
-function hello_dolly_get_lyric() {
-	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-So, take her wrap, fellas
-Find her an empty lap, fellas
-Dolly'll never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-Golly, gee, fellas
-Find her a vacant knee, fellas
-Dolly'll never go away
-Dolly'll never go away
-Dolly'll never go away again";
+function focus_hocus_pocus() {
+	/** These are the lyrics to Focus - Hocus Pocus */
+	$lyrics = "Yodeadodoyodeadodoyodeadodoyodeadodo
+yodeadodoyodeadodoyo-bab-baaaaa
+Ahhhhhh-aaahhhh-aaaaaa-aaaaAAA!
+Ohhhhhh-ooohhh-oooooo-oooOOO!
 
-	// Here we split it into lines
+Yodeadodoyodeadodoyodeadodoyodeadodo
+yodeadodoyodeadodoyo-bab-baaaaa
+Ahhhhhh-aaahhhh-aaaaaa-aaaaAAA!
+Ohhhhhh-ooohhh-oooooo-oooOOO!
+
+umdub-adaoh-segel-ungucur-ungetu-hungetur-hupreyu
+undubea-unpedurl-humpelilly-luptodoro-licktetor-ulumpadero
+umbader-lickatine-lupator-lackatera
+batickatheplalera
+theblumpalumpadera
+ho?
+ho ho ha haaaa!
+
+Yodeadodoyodeadodoyodeadodoyodeadodo
+yodeadodoyodeadodoyo-bab-baaaaa
+Ahhhhhh-aaahhhh-aaaaaa-aaaaAAA!
+Ohhhhhh-ooohhh-oooooo-oooOOO!
+
+Bom bom bom bom
+Bom bom bom
+Bom bom bom bom bom bom bom
+Bom bom bom
+Bom bom bom
+Bom bom bom bom bom bom
+Bac bac bac bac bac backaaaaa!
+Yeeeeha!
+
+ba um um um um um
+ba um um um um um
+ba oooohhboooobooboboboooo!
+
+Yodeadodoyodeadodoyodeadodoyodeadodo
+yodeadodoyodeadodoyo-bab-baaaaa
+Ahhhhhh-aaahhhh-aaaaaa-aaaaAAA!
+Ohhhhhh-ooohhh-oooooo-oooOOO!
+Yeaah! Whoooo!";
+
+	// Split the lyrics into lines
 	$lyrics = explode( "\n", $lyrics );
 
 	// And then randomly choose a line
@@ -51,22 +64,22 @@ Dolly'll never go away again";
 }
 
 // This just echoes the chosen line, we'll position it later
-function hello_dolly() {
-	$chosen = hello_dolly_get_lyric();
-	echo "<p id='dolly'>$chosen</p>";
+function focus() {
+	$chosen = focus_hocus_pocus();
+	echo "<p id='focus'>$chosen</p>";
 }
 
 // Now we set that function up to execute when the admin_notices action is called
-add_action( 'admin_notices', 'hello_dolly' );
+add_action( 'admin_notices', 'focus' );
 
 // We need some CSS to position the paragraph
-function dolly_css() {
+function focus_css() {
 	// This makes sure that the positioning is also good for right-to-left languages
 	$x = is_rtl() ? 'left' : 'right';
 
 	echo "
 	<style type='text/css'>
-	#dolly {
+	#focus {
 		float: $x;
 		padding-$x: 15px;
 		padding-top: 5px;		
@@ -77,6 +90,6 @@ function dolly_css() {
 	";
 }
 
-add_action( 'admin_head', 'dolly_css' );
+add_action( 'admin_head', 'focus_css' );
 
 ?>
